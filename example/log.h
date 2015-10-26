e05a3f4d53f9076173bb73db080a4908404b9cb0
lixiangx <lixiangx@diditaxi.com.cn>
2015-10-26 15:57:59 +0800

    del some log

--- example/log.h
+++ example/log.h
@@ -14,24 +14,13 @@
 
 
 /* 每个线程的buffer size*/
-#define   _LOG_BUFFSIZE  1024*1024*4
-/* 当前进程的 Stream IO buffer size*/
-#define   _SYS_BUFFSIZE  1024*1024*8
+#define   _LOG_BUFFSIZE  40*1024
 /* log 文件字符串路径最大长度*/
 #define	  _LOG_PATH_LEN  250
 /* 日志对应的模块名*/
 #define   _LOG_MODULE_LEN 32
 
-typedef  enum LogLevel {  
-	LL_DEBUG = 1,
-	LL_TRACE = 2,
-	LL_NOTICE = 3, 
-	LL_WARNING = 4, 
-	LL_ERROR = 5,
-    LL_PUBLIC = 6,
-}LogLevel;
 
-// ??д??
 class RWLock
 {
 public:
@@ -60,14 +49,13 @@ private:
 
 
 /**
-*	Log_Writer  日志类
+*	LogWriter  日志类
 */
-class Log_Writer : public google::base::Logger
+class LogWriter : public google::base::Logger
 {
 	public:
-		Log_Writer()
+		LogWriter()
 		{
-			m_system_level = LL_NOTICE;
 			//fp = stderr;
 			fp = NULL;
 			m_issync = false;
@@ -76,19 +64,16 @@ class Log_Writer : public google::base::Logger
 			m_node = 0;
 			//pthread_mutex_init(&m_mutex, NULL);
 		}
-		~Log_Writer(){
-			logclose();
+		~LogWriter(){
+			Destroy();
 		}
-		bool loginit(LogLevel l, const  char *filelocation, bool append = true, bool issync = false);
-		bool log(LogLevel l,const char *logformat,...);
-		LogLevel get_level();
-		bool logclose();
+		bool Init(const  char *filelocation, const char* level, bool append = true, bool issync = false);
+		bool Log(const char *logformat,...);
+		bool Destroy();
 	private:
-		const char* logLevelToString(LogLevel l);
-		bool checklevel(LogLevel l);
-		int premakestr(char* m_buffer, LogLevel l);
-		uint64_t file_node();
-		bool _write( char *_pbuffer, int len);
+		int PreMakeStr();
+		uint64_t FileNode();
+		bool Write( char *_pbuffer, int len);
 		virtual void Write(bool /* should_flush */,
                  time_t /* timestamp */,
                  const char* message,
@@ -96,31 +81,18 @@ class Log_Writer : public google::base::Logger
         virtual void Flush();
         virtual uint32_t LogSize();
 	private:
-		enum LogLevel m_system_level;
 		FILE* fp;
 		bool m_issync;
 		bool m_isappend;
 		char m_filelocation[_LOG_PATH_LEN];
-		//pthread_mutex_t m_mutex;
 		RWLock m_lock;
 		static __thread char m_buffer[_LOG_BUFFSIZE];
 		uint64_t m_node;
-		//The __thread specifier may be applied to any global, file-scoped static, function-scoped static, 
-		//or static data member of a class. It may not be applied to block-scoped automatic or non-static data member
-		//in the log  scence,It's safe!!!!
-		//一言以蔽之，此场景不用担心__thread带来资源leak,同时也不用担心多个Log_Writer会干扰，
-		//因为一个线程同一时间只有一个Log_Writer在干活，干完之后m_buffer就reset了
-		//所以即便一个线程用户多个Log_Write串行(因为一个线程内的运行态只有串行) 也是线程安全的！！！
+		std::string m_pre_level;
 };
 
-extern Log_Writer PUBLIC_W;
-extern Log_Writer WARN_W;
-extern Log_Writer INFO_W;
-
-void set_uuid(uint64_t log_id = 0);
-void set_pid(int pid);
-uint64_t get_uuid();
-
+void SetUid(uint64_t log_id = 0);
+uint64_t GetUid();
 
 
 /**
@@ -128,6 +100,6 @@ uint64_t get_uuid();
  * p_modulename 模块名 如mysql
  * p_logdir  日志输出目录
  * */
-bool log_init(LogLevel l, const char* p_modulename, const char* p_logdir);
+bool log_init(const char* p_modulename, const char* p_logdir);
 bool log_close();
 #endif
